c8451106ffd8b27603a47c7008e2b21bb1ee6508
YazTepRod <sytepale@gmail.com>
2020-08-08 07:17:00 -0500

    Add components

---
+++ src/components/login.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import login from './components/login';
+
+
+function Login() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Bienvenido a Lab-Notes
+          </p>
+         
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
+  
