03c517679a653cc983bfbaf5bcb247201c6b67f7
YazTepRod <sytepale@gmail.com>
2020-08-18 09:08:06 -0500

    Add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido a Lab-Notes
-        </p>
-       
-      </header>
-=======
 import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 import SingIn from './components/SingIn';
 //import SingUp from './components/SingUp';
 import Notes from './components/Notes';
 import { useFirebaseApp } from 'reactfire';
 import {  useUser } from 'reactfire';
-
 
 
 function App() {
@@ -44,7 +30,7 @@ function App() {
      </Switch>
    </div>
    </Router>
-   :<span>
+   <span>
    <SingIn/>
    <Router>
     <div className="container">
@@ -52,8 +38,7 @@ function App() {
         <Route path="/singIn">
           <SingIn />
         </Route>
-        <Route path="/singUp">
-          
+        <Route path="/singUp">   
         </Route>
         <Route path="/notes">
           <Notes />
@@ -63,7 +48,6 @@ function App() {
     </Router>
     </span>
    }
->>>>>>> e05b26f4be5904110047981a7c05c7495f75daf0
     </div>
   );
 }
